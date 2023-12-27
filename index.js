7335e78bd6eac9f7658822cb06eb7c7430d1b56c
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2023-12-27 14:39:44 +0530

    ./

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const connectDB = require("./db");
 const router = require("./routes/userRoutes");
 
 const app = express();
+
+app.use(express.json());
+app.use(
+  "*",
+  cors({
+    origin: "*",
+    credentials: true,
+    headers:
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    methods: "GET, POST, PATCH, PUT, POST, DELETE, OPTION",
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 dotenv.config();
 connectDB();
-app.use(express.json());
 
 app.use("/api/v1/user", router);
 
